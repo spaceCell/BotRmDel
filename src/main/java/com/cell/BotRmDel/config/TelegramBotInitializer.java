40a9d0b5728ad42d575712f9c450973fbb33777a
Kirill Kozlov <space_cell@proton.me>
2023-12-09 13:39:40 +0500

    Add configuration on Bot

---
+++ src/main/java/com/cell/BotRmDel/config/TelegramBotInitializer.java
@@ -0,0 +1,27 @@
+package com.cell.BotRmDel.config;
+
+import com.cell.BotRmDel.model.BotRmDel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class TelegramBotInitializer {
+
+    @Autowired
+    private BotRmDel bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException telegramApiException) {
+            telegramApiException.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/cell/BotRmDel/model/BotRmDel.java
@@ -0,0 +1,29 @@
+package com.cell.BotRmDel.model;
+
+import com.cell.BotRmDel.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class BotRmDel extends TelegramLongPollingBot {
+
+    @Autowired
+    private BotConfig botConfig;
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
