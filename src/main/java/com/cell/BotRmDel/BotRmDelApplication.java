79c962dd39bd13c671269d33ad66cd1b9de7fe02
Kirill Kozlov <space_cell@proton.me>
2023-12-09 21:18:47 +0500

    Start App

---
+++ src/main/java/com/cell/BotRmDel/BotRmDelApplication.java
@@ -0,0 +1,13 @@
+package com.cell.BotRmDel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotRmDelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotRmDelApplication.class, args);
+	}
+
+}
